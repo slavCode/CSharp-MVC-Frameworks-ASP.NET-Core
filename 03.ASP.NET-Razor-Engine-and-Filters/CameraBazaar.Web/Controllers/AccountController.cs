088facfd16d15d5d0a01f1a36feb459f1b7e7917
Slav Tomov <stomow@gmail.com>
2018-05-23 23:41:26 +0300

    Refactor

--- 03.ASP.NET-Razor-Engine-and-Filters/CameraBazaar.Web/Controllers/AccountController.cs
+++ 03.ASP.NET-Razor-Engine-and-Filters/CameraBazaar.Web/Controllers/AccountController.cs
@@ -41,7 +41,6 @@
         [AllowAnonymous]
         public async Task<IActionResult> Login(string returnUrl = null)
         {
-            // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
             ViewData["ReturnUrl"] = returnUrl;
