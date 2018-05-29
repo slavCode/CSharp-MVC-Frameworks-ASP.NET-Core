594ca59ca3d80dbe2ef1adb03e88073a6438bbe1
Slav Tomov <stomow@gmail.com>
2018-05-30 00:07:10 +0300

    Refactor

--- 05.ASP.NET-Web-API/BookShop.Service/Infrastructure/StringExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookShop.Service.Infrastructure
-{
-    public static class StringExtensions
-    {
-        public static string Capitalize(this string word)
-            => word[0].ToString().ToUpper() + word.Substring(1);
-    }
-}
